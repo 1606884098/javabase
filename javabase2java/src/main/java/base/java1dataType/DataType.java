fb6790fec19bf8ef560fea2a0faca882abe80204
1606884098 <1606884098@qq.com>
2019-01-21 12:26:48 +0800

    git提交规则

--- javabase2java/src/main/java/base/java1dataType/DataType.java
+++ javabase2java/src/main/java/base/java1dataType/DataType.java
@@ -2,6 +2,6 @@ package base.java1dataType;
 
 public class DataType {
     public static void main(String[] args) {
-        System.out.print("测试一个");
+        System.out.print("测试一个提交规则");
     }
 }
