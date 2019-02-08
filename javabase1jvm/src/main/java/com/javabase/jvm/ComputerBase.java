b09cb41f78f4bfd87d9afd9402ea434537fc2dc9
1606884098 <1606884098@qq.com>
2019-02-08 22:02:15 +0800

    注释

---
+++ javabase1jvm/src/main/java/com/javabase/jvm/ComputerBase.java
@@ -0,0 +1,19 @@
+package com.javabase.jvm;
+
+/**
+ * 计算机基础知识：
+ *     计算机的文字，图片，视频等数据都是二进制存储运算的。
+ * jvm基础：
+ *      jre=jvm+java核心类库
+ *      jvm=jdk1.8.0_171\jre\bin\server\jvm.dll就是java虚拟机 java核心类库=jdk1.8.0_171\jre\lib\rt.jar
+ *      jdk=jre+java开发工具包
+ *      java规范：
+ *      1.jvm规范：定了二进制class文件和jvm指令集，运行
+ *      2.java语言规范：定义了什么是java语言
+ *      .class文件是可以运行在任何支持Java虚拟机的硬件平台或者java虚拟机的操作系统上的二进制文件
+ */
+public class ComputerBase {
+    public static void main(String[] args) {
+
+    }
+}
