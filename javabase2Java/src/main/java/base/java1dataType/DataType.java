86100a73f23f0145943d08bc59885c7fec9bbc4e
1606884098 <1606884098@qq.com>
2019-02-17 12:27:38 +0800

    new

--- javabase2Java/src/main/java/base/java1dataType/DataType.java
+++ javabase2Java/src/main/java/base/java1dataType/DataType.java
@@ -2,6 +2,21 @@ package base.java1dataType;
 
 public class DataType {
     public static void main(String[] args) {
-        System.out.print("测试一个提交规则");
+        /**
+         * 1.基本数据类型
+         *  1.1整数型
+         *      byte 1b 8bit -128 ~ 127 默认值0
+         *      Short 2b 16bit -32768 ~ 32678 默认值0
+         *      Int 4b 32bit  -2,147,483,648 ~ 2,147,483,647 默认值0
+         *      long 8bit 64bit  -9,223,372,036,854,775,808~+9,223,372,036,854,775,807 默认值0L
+         *  1.2浮点型
+         *      float 4b 32bit  -3,40292347E+38 ~ +3,40292347E+38 默认值0
+         *      double 8b 64bit -1.79769313486231576E+308 ~ 1.79769313486231576E+308 默认值0
+         *  1.3布尔类型
+         *      boolean 1b 8bit true/false 默认值false
+         *  1.4字符
+         *      char 2b 16bit 默认值为空 /u0000 是一个空的字符，它与null和""不同，是特殊的“空”
+         *
+         */
     }
 }
---
+++ javabase2Java/src/main/java/base/java3oop/oop1notion/Oop1Notion.java
@@ -0,0 +1,7 @@
+package base.java3oop.oop1notion;
+
+/**
+ * 说明 enum也在这里讲
+ */
+public class Oop1Notion {
+}
