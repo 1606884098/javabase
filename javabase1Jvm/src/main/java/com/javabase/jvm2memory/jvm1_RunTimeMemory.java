5be9ac15f55819d80d2301e2b40719ad8d2ec956
yeming <1606884098@qq.com>
2019-07-18 09:01:59 +0800

    添加jvm内容

---
+++ javabase1Jvm/src/main/java/com/javabase/jvm2memory/jvm1_RunTimeMemory.java
@@ -0,0 +1,9 @@
+package com.javabase.jvm2memory;
+
+/**
+ * 1.1.运行时数据区
+ *    JVM运行时数据区(JVM Runtime Area)其实就是指JVM在运行期间，其对计算机内存空间的划分和分配。
+ * 本文将通过以下几个话题来讨论JVM运行时数据区
+ */
+public class jvm1_RunTimeMemory {
+}
