957859df420516e980558fc300b20b181e0f0761
yeming <1606884098@qq.com>
2019-08-02 09:08:56 +0800

    添加jvm知识

---
+++ javabase1Jvm/src/main/java/com/javabase/jvm1runtime/runtimeDetail/JvmNotionDetail.java
@@ -0,0 +1,14 @@
+package com.javabase.jvm1runtime.runtimeDetail;
+
+/**
+ * 1.虚拟机
+ * 2.jvm规范
+ * 2.1java语言规范
+ * Java语言的规范主要是定义使用的规范和实现机制，也就是定义了说明是java语言
+ * 比如：if语句
+ * 参数列表
+ *2.2jvm规范
+ * 主要是虚拟机运行规范（二进制的处理）
+ */
+public class JvmNotionDetail {
+}
