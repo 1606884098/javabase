63b9b92ec77addb828ecea2684490ccf181eead2
Administrator <1606884098@qq.com>
2019-08-04 23:05:35 +0800

    添加jvm知识

--- javabase1Jvm/src/main/java/com/javabase/jvm1runtime/runtimeDetail/JvmNotionDetail.java
+++ javabase1Jvm/src/main/java/com/javabase/jvm1runtime/runtimeDetail/JvmNotionDetail.java
@@ -3,12 +3,16 @@ package com.javabase.jvm1runtime.runtimeDetail;
 /**
  * 1.虚拟机
  * 2.jvm规范
- * 2.1java语言规范
- * Java语言的规范主要是定义使用的规范和实现机制，也就是定义了说明是java语言
+ * 2.1jvm语言规范
+ * 符合jvm语言规范的语言有很多种比如java,scala，编译后都能在jvm上运行
+ * Java语言的规范主要是定义使用的规范和实现机制，也就是定义了什么是java语言
+ * 图2.1语法定义
  * 比如：if语句
  * 参数列表
- *2.2jvm规范
- * 主要是虚拟机运行规范（二进制的处理）
+ * 2.2jvm规范
+ * 主要是虚拟机运行规范（二进制的处理），直接执行的是jvm指令代码
+ * 3.class文件的结构
+ *  java 文件编译规则：就是将java文件按照3系列图的规则编译成.class文件
  */
 public class JvmNotionDetail {
 }
