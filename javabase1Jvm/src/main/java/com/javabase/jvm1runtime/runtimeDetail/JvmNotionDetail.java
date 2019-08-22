b4dc9b377b14555adc392a7947ced225689439a3
Administrator <1606884098@qq.com>
2019-08-22 22:24:59 +0800

    整理jvm讲义

--- javabase1Jvm/src/main/java/com/javabase/jvm1runtime/runtimeDetail/JvmNotionDetail.java
+++ javabase1Jvm/src/main/java/com/javabase/jvm1runtime/runtimeDetail/JvmNotionDetail.java
@@ -1,18 +1,40 @@
 package com.javabase.jvm1runtime.runtimeDetail;
 
 /**
+ *
  * 1.虚拟机
+ * JVM是Java Virtual Machine的简称。意为Java虚拟机
+ * 虚拟机
+ * 指通过软件模拟的具有完整硬件系统功能的、运行在一个完全隔离环境中的完整计算机系统
+ * 有哪些虚拟机
+ * VMWare
+ * Visual Box
+ * JVM
+ * VMWare或者Visual Box都是使用软件模拟物理CPU的指令集
+ * JVM使用软件模拟Java 字节码的指令集
+ *
  * 2.jvm规范
  * 2.1jvm语言规范
+ * 语法
+ * 变量
+ * 类型
+ * 文法
  * 符合jvm语言规范的语言有很多种比如java,scala，编译后都能在jvm上运行
  * Java语言的规范主要是定义使用的规范和实现机制，也就是定义了什么是java语言
  * 图2.1语法定义
  * 比如：if语句
  * 参数列表
  * 2.2jvm规范
+ * Class文件类型
+ * 运行时数据
+ * 帧栈
+ * 虚拟机的启动
+ * 虚拟机的指令集
  * 主要是虚拟机运行规范（二进制的处理），直接执行的是jvm指令代码
  * 3.class文件的结构
  *  java 文件编译规则：就是将java文件按照3系列图的规则编译成.class文件
+ *
+ *
  */
 public class JvmNotionDetail {
 }
