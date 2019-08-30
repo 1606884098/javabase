01e2a2714d22abf395bdfae0e0fcbc63bbc51a9b
yeming <1606884098@qq.com>
2019-08-30 16:54:10 +0800

    添加TEST

--- javabase1Jvm/src/main/java/com/javabase/TEST.java
+++ javabase1Jvm/src/main/java/com/javabase/TEST.java
@@ -1,5 +1,6 @@
 package com.javabase;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.LongAdder;
 
 /**
@@ -86,9 +87,49 @@ public class TEST {
         System.out.println(A.equals(B));
 
         /**
+         *    private static final Unsafe unsafe = Unsafe.getUnsafe();
+         *     private static final long valueOffset;//value这个值的内存地址方便查找
          *
+         *     static {
+         *         try {
+         *             valueOffset = unsafe.objectFieldOffset
+         *                 (AtomicInteger.class.getDeclaredField("value"));
+         *         } catch (Exception ex) { throw new Error(ex); }
+         *     }
+         *
+         *     private volatile int value;//AtomicInteger当前的值
+         *
+         *     volatile：禁止指令的重排序优化，对volatile变量所有的写操作都能立即反应到其他线程中，换句话说，
+         *     volatile变量在各个线程中是一致的（得益于java内存模型—"先行发生原则"）；
+         *
+         *    CAS:Compare-And-Swap比较并交换见CAS.png
+         *
+         *     public final int incrementAndGet() {
+         *         for (;;) {
+         *             int current = get();//获取到的是value的值
+         *             int next = current + 1;//需要更新的值
+         *             if (compareAndSet(current, next))// 如果内存的值也就是valueOffset地址的值，如果和预期值也就是value的中相同说明没有更新，更新成为 需要更新的值，如果与预期的值不一样，就会将value值改成预期的值。
+         *                 return next;//返回根系的值
+         *         }
+         *    }
+         *
+         *   CAS的ABA问题
+         * （1）线程1读取出指定内存地址的数据A，加载到寄存器，此时读取出来的原值不仅将作为要被计算的值A，还会作为比较值A。
+         * （2）此时线程1的cpu被线程2抢占了，线程2也从同样的内存地址中读取了同样的数据A，线程2还比线程1先执行完，线程2产
+         *      生了新数据B，并且遵守了CAS原理把新数据B存入该内存地址。 （这个时候内存的值由A被该为B）
+         * （3）还没完，线程2执行完之后，线程1又没抢过其它线程，此时cpu被线程3抢占，之后步骤和第（2）步一样，线程3从同样
+         *      的内存地址中读取了数据B，线程3还比线程1先执行完，线程3产生了新数据A（没错，与一开始的A相等，但值相等并不
+         *      意味着此A就是彼A，已经被替换了），并且遵守了CAS原理把新数据A存入该内存地址。（这个时候内存的值由B又变为A）
+         *
+         *      ABA问题  对库存类的数据不影响，因为整体AtomicInteger的值是没有改变的
          */
-
+        AtomicInteger atomicInteger=new AtomicInteger();
+        atomicInteger.addAndGet(1);
+        /**
+         *  LongAdder在AtomicLong的基础上将单点的更新压力分散到各个节点，在低并发的时候通过对base的直接更新可以很好的保障
+         *   和AtomicLong的性能基本保持一致，而在高并发的时候通过分散提高了性能。
+         *  缺点是LongAdder在统计的时候如果有并发更新，可能导致统计的数据有误差。
+         */
         LongAdder longAdder=new LongAdder();
         longAdder.add(1l);
         System.out.println(longAdder);
